e015df61b0366e1ce8f51e64bc7b0a2df87fd04c
MI-Cloud <andy_erpconsulting@outlook.com>
2026-04-30 10:20:42 +0530

    Print Fomate & DImesnion

---
+++ vecmocon_customization/vecomocon/doctype/business_location/business_location.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, MI_Cloud and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Business Location", {
+// 	refresh(frm) {
+
+// 	},
+// });
