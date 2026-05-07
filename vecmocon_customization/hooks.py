d61794dd387078f5d5618c3d3e5320ce2e4ebb15
MI-Cloud <andy_erpconsulting@outlook.com>
2026-05-07 13:54:26 +0530

    Update Report of Stock Entry

--- vecmocon_customization/hooks.py
+++ vecmocon_customization/hooks.py
@@ -352,7 +352,8 @@ fixtures = [
         "dt": "Print Format",
         "filters": [
             ["name", "in", ["Purchase Order", "Purchase Receipt", "Purchase Invoice", "Tax-Invoice", "Delivery Challan", 
-            "Delivery Challan Without Values", "Goods Sales Order", "Services Sales Order", "SE Delivery Challan", "SE Delivery Challan Without Values"]]
+            "Delivery Challan Without Values", "Goods Sales Order", "Services Sales Order", "SE Delivery Challan", "SE Delivery Challan Without Values",
+            "Material Transfer Without Value", "Material Transfer With Value"]]
         ]
     },
 ]
