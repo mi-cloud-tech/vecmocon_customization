d0f024f6275a640bd8b451f608210e9ffc5498f2
MI-Cloud <andy_erpconsulting@outlook.com>
2026-05-22 10:18:24 +0530

    Update in Prod

--- vecmocon_customization/override/purchase_invoice.py
+++ vecmocon_customization/override/purchase_invoice.py
@@ -1,19 +1,39 @@
 import frappe
 from frappe.utils import getdate
 
-def purchase_invoice_before_save(self, method):
-    date = None
-    pr = self.get("items",{"purchase_receipt": ["!=", ""]})
-    if len(pr) > 1:
-        frappe.throw("Multiple Purchase Receipts are not allowed in a single Purchase Invoice.")
 
+def purchase_invoice_before_save(self, method):
+
+    pr_list = set()
+    pr_date = None
+
+    # Collect unique Purchase Receipts
     for item in self.items:
         if item.purchase_receipt:
-            date = frappe.get_value("Purchase Receipt", item.purchase_receipt, "posting_date")
-            break
-    
-    if date and getdate(self.posting_date) < getdate(date):
-        frappe.throw("Purchase Invoice posting date cannot be earlier than the linked Purchase Receipt posting date.")
+            pr_list.add(item.purchase_receipt)
 
-    if date:
-        self.custom_purchase_receipt_date = date
+    # Prevent multiple Purchase Receipts
+    if len(pr_list) > 1:
+        frappe.throw(
+            "Multiple Purchase Receipts are not allowed in a single Purchase Invoice."
+        )
+
+    # Get Purchase Receipt date
+    if pr_list:
+        purchase_receipt = list(pr_list)[0]
+
+        pr_date = frappe.db.get_value(
+            "Purchase Receipt",
+            purchase_receipt,
+            "posting_date"
+        )
+
+    # Validate Purchase Invoice date
+    if pr_date and getdate(self.posting_date) < getdate(pr_date):
+        frappe.throw(
+            "Purchase Invoice posting date cannot be earlier than the linked Purchase Receipt posting date."
+        )
+
+    # Store PR date in custom field
+    if pr_date:
+        self.custom_purchase_receipt_date = pr_date
